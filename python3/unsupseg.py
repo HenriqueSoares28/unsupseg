ce0ee1b64325f28a56a0a29e6b9df2cfe9f5c372
Henrique França Carvalho Soares <69248621+HenriqueSoares28@users.noreply.github.com>
2024-06-03 12:12:19 -0300

    Refactor load_image method to convert image to grayscale

--- python3/unsupseg.py
+++ python3/unsupseg.py
@@ -9,7 +9,9 @@ class Unsupseg:
     def run(self, type, image, method, k, pol, SPsize):
         subprocess.run([self.command, type, image, method, k, pol, SPsize])
 
-    def load_image(self,  image_path):
-        img = Image.open(image_path).convert('I')
-        img_array = np.array(img)
+    def load_image(self, image_path):
+        with Image.open(image_path) as img:
+            print(f"Image mode: {img.mode}, max pixel value: {img.getextrema()}")
+            img = img.convert('F')  
+            img_array = np.array(img)
         return img_array
